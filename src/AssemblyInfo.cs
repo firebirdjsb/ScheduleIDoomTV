602db3883bbf871d78532922bb3756754a88852e
firebirdjsb <75272969+firebirdjsb@users.noreply.github.com>
2026-08-10 18:31:49 -0400

    Add valid MelonLoader assembly metadata

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using ScheduleIDoomTV;
+
+[assembly: MelonInfo(typeof(DoomTvMod), "Schedule I - Doom TV", "0.2.1-alpha", "Justin + OpenAI")]
+[assembly: MelonGame(null, "Schedule I")]
