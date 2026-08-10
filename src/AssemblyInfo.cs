642048cb7e22d89b6dc46a1dc1f384b210633c29
firebirdjsb <75272969+firebirdjsb@users.noreply.github.com>
2026-08-10 19:00:40 -0400

    Bump TV app integration build version

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using MelonLoader;
 using ScheduleIDoomTV;
 
-[assembly: MelonInfo(typeof(DoomTvMod), "Schedule I - Doom TV", "0.2.1-alpha", "Justin + OpenAI")]
+[assembly: MelonInfo(typeof(DoomTvMod), "Schedule I - Doom TV", "0.2.2-alpha", "Justin + OpenAI")]
 [assembly: MelonGame(null, "Schedule I")]
